15bd77b317b06502bda4392c6c6597315059361b
Pablo Novas <pjnovas@gmail.com>
2015-12-27 21:58:52 -0300

    added custom sort, article body and maps

--- client/components/Article.jsx
+++ client/components/Article.jsx
@@ -5,8 +5,16 @@ class Article extends React.Component {
     let article = this.props.article;
     return (
       <div className="article-view">
-        {article.number} - {article.title}
-
+        {article.hasMap ?
+          <div className="article-map">
+            <iframe width="100%" height="520" frameBorder="0" src={article.map.url} allowFullScreen>
+            </iframe>
+          </div>
+        : null }
+        <div className="article-content">
+          <h1>{article.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: article.body}}></div>
+        </div>
       </div>
     );
   }
