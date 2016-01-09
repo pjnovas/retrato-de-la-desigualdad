2ed6ac2d0ee5c685bbc57df7669863d2654213b8
Pablo Novas <pjnovas@gmail.com>
2016-01-08 22:56:06 -0300

    added metadata form

--- client/Router.jsx
+++ client/Router.jsx
@@ -4,11 +4,13 @@ import createBrowserHistory from 'history/lib/createBrowserHistory';
 import { render } from 'react-dom';
 
 import Landing from "./components/index.jsx";
+import Metadata from "./components/Metadata.jsx";
 import SectionView from "./components/SectionView.jsx";
 
 const routes = (
   <Router history={createBrowserHistory()}>
     <Route path="/" component={Landing} />
+    <Route path="/metadata" component={Metadata} />
     <Route path="/:section" component={SectionView} />
     <Route path="/:section/:article" component={SectionView} />
   </Router>
---
+++ client/components/Metadata.jsx
@@ -0,0 +1,179 @@
+
+const metaStart = "[#meta]";
+const metaEnd = "[/meta]";
+
+const defaultState = {
+  metas: {
+    coords: ["",""],
+    cando: "",
+    prices: "",
+    distance: "",
+    groups: { a: false, b: false },
+    times: { day: false, night: false }
+  },
+
+  parsed: "",
+  error: ""
+};
+
+class Metadata extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = Object.create(defaultState);
+  }
+
+  onChange(p, v){
+    let metas = this.state.metas;
+    metas[p] = v;
+    this.setState({ metas }, () => this.generate());
+  }
+
+  onChangeCoords(idx, value){
+    let metas = this.state.metas;
+    metas.coords[idx] = value;
+    this.setState({ metas }, () => this.generate());
+  }
+
+  onChangeCheck(type, index, value){
+    let metas = this.state.metas;
+    metas[type][index] = value;
+    this.setState({ metas }, () => this.generate());
+  }
+
+  generate(){
+    let parsed = metaStart + JSON.stringify(this.state.metas) + metaEnd;
+    this.setState({ parsed, error: "" });
+  }
+
+  onChangeParsed(parsed){
+    this.setState({ parsed, error: "" }, () => this.parse());
+  }
+
+  parse(){
+    let body = this.state.parsed;
+    let len = metaStart.length;
+
+    let idxStart = body.indexOf(metaStart);
+    let idxEnd = body.indexOf(metaEnd);
+
+    if (idxStart === -1){
+      this.setState({ error: "No se encontro el inicio '[#meta]'" });
+      return;
+    }
+
+    if (idxEnd === -1){
+      this.setState({ error: "No se encontro el fin '[/meta]'" });
+      return;
+    }
+
+    let metas;
+
+    try {
+      metas = JSON.parse(body.substring(idxStart + len, idxEnd));
+    }
+    catch (e) {
+      this.setState({ error: "Ocurrió un error al Parsear el texto"});
+      console.dir(e);
+      return;
+    }
+
+    this.setState({ metas, error: "" });
+  }
+
+  cleanAll(){
+    //this.setState(defaultState);
+    window.location.reload();
+  }
+
+  render() {
+    let metas = this.state.metas;
+
+    return (
+      <div className="metadata">
+        <h1>Metadata</h1>
+
+        <div className="control">
+          <label>Coordenadas</label>
+          <input className="coord" type="text"
+            placeholder="LATITUD" value={metas.coords[0]}
+            onChange={e=>this.onChangeCoords(0, e.target.value)}/>
+          <input className="coord" type="text"
+            placeholder="LONGITUD" value={metas.coords[1]}
+            onChange={e=>this.onChangeCoords(1, e.target.value)}/>
+        </div>
+
+        <div className="control">
+          <label>Se puede</label>
+          <textarea value={metas.cando}
+            onChange={e=>this.onChange("cando", e.target.value)}/>
+        </div>
+
+        <div className="control">
+          <label>Precios referencia</label>
+          <textarea value={metas.prices}
+            onChange={e=>this.onChange("prices", e.target.value)}/>
+        </div>
+
+        <div className="control">
+          <label>Distancia desde El Salvador del Mundo</label>
+          <input type="text" value={metas.distance} className="distance"
+            onChange={e=>this.onChange("distance", e.target.value)}/>
+        </div>
+
+        <div className="control">
+          <label>Grupo que lo visita</label>
+
+          <div className="check">
+            <label htmlFor="groups_a">Grupo A</label>
+            <input id="groups_a" type="checkbox" checked={metas.groups.a}
+              onChange={e=>this.onChangeCheck("groups", "a", e.target.checked)}/>
+          </div>
+
+          <div className="check">
+            <label htmlFor="groups_b">Grupo B</label>
+            <input id="groups_b" type="checkbox" checked={metas.groups.b}
+              onChange={e=>this.onChangeCheck("groups", "b", e.target.checked)}/>
+          </div>
+        </div>
+
+        <div className="control">
+          <label>Tiempo de visita</label>
+
+          <div className="check">
+            <label htmlFor="times_day">Día</label>
+            <input id="times_day" type="checkbox" checked={metas.times.day}
+              onChange={e=>this.onChangeCheck("times", "day", e.target.checked)}/>
+          </div>
+
+          <div className="check">
+            <label htmlFor="times_night">Noche</label>
+            <input id="times_night" type="checkbox" checked={metas.times.night}
+              onChange={e=>this.onChangeCheck("times", "night", e.target.checked)}/>
+          </div>
+        </div>
+
+        <hr/>
+
+        {this.state.error ?
+          <div>
+            <p className="error">{this.state.error}</p>
+            <hr/>
+          </div>
+        : null }
+
+        <textarea className="parsed" value={this.state.parsed}
+          placeholder="Pegar metadata para modificar"
+          onChange={e=>this.onChangeParsed(e.target.value)}/>
+
+        <div className="control">
+          <a onClick={ () => this.cleanAll() }>Limpiar Todo</a>
+        </div>
+      </div>
+    );
+  }
+
+};
+
+Metadata.displayName = "Metadata";
+export default Metadata;
